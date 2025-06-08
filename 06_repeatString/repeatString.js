7d88a4bf00843f784821b2be91f4513c8bc6c80a
Dawson <dawsonjpeck@gmail.com>
2025-06-08 09:26:07 -0400

    add 06 solution

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function (word, count) {
+  let repeatedString = "";
 
+  if (count < 0) {
+    return "ERROR";
+  }
+
+  for (let i = 0; i < count; i++) {
+    repeatedString += word;
+  }
+  return repeatedString;
 };
 
 // Do not edit below this line
