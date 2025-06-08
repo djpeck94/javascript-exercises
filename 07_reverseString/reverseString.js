9654d3459905c62539e78c1491b5f494f0ede980
Dawson <dawsonjpeck@gmail.com>
2025-06-08 09:38:19 -0400

    add solution for 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function (startingString) {
+  let stringArray = startingString.split("");
+  let reversedArray = stringArray.reverse();
+  let newString = reversedArray.join("");
+  return newString;
 };
 
 // Do not edit below this line
