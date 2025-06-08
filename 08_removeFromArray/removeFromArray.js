ee3c703b33a85df19fe401919d3e10010df26b51
Dawson <dawsonjpeck@gmail.com>
2025-06-08 12:02:02 -0400

    add 08 solution

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,25 @@
-const removeFromArray = function() {
+const removeFromArray = function (arrayOne, ...arrayTwo) {
+  // take an element out of arrayTwo
+
+  //search arrayOne for that element
+
+  //remove element from arrayOne if it exists
+
+  //repeat until arrayTwo is empty
+
+  //return new Array
+
+  while (arrayTwo.length > 0) {
+    let num = arrayTwo.pop();
+    arrayOne.forEach(() => {
+      let numIndex = arrayOne.indexOf(num);
+      if (numIndex >= 0) {
+        arrayOne.splice(numIndex, 1);
+      }
+    });
+  }
+
+  return arrayOne;
 };
 
 // Do not edit below this line
