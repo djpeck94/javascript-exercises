bc0775c038f8fd9cb405ab9b345435f2ae4be963
Dawson <dawsonjpeck@gmail.com>
2025-06-09 20:19:23 -0400

    add 10 solution

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
+const leapYears = function (myYear) {
+  // Gregorian rule: every 400th year *is* a leap year.
+  if (myYear % 400 === 0) return true;
 
+  // Otherwise, years divisible by 100 are *not* leap years.
+  if (myYear % 100 === 0) return false;
+
+  // All other years that are divisible by 4 are leap years.
+  return myYear % 4 === 0;
 };
 
 // Do not edit below this line
