21f18298e447c631864fbc0c657c0a9222d8f26e
Dawson <dawsonjpeck@gmail.com>
2025-06-09 20:03:02 -0400

    add 09 solution

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,33 @@
-const sumAll = function() {
+const sumAll = function (numOne, numTwo) {
+  let sum = 0;
 
+  let storeNums = [numOne, numTwo];
+
+  storeNums.sort((a, b) => a - b);
+  numOne = storeNums[0];
+  numTwo = storeNums[1];
+
+  if (
+    numOne < 0 ||
+    numTwo < 0 ||
+    numOne % 1 != 0 ||
+    numTwo % 1 != 0 ||
+    typeof numOne != "number" ||
+    typeof numTwo != "number"
+  ) {
+    return "ERROR";
+  }
+
+  let numArray = [];
+  for (let i = numOne; i <= numTwo; i++) {
+    numArray.push(i);
+  }
+
+  for (let j = 0; j < numArray.length; j++) {
+    sum += numArray[j];
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
